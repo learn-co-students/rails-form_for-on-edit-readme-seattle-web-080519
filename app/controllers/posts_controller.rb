da829e0d15ba257e405520526746dd300fc17fb7
github username <github email address>
2019-09-02 22:31:35 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
 	#@post.update(title: params[:title], description: params[:description])
 	#Because 'form_for' is bound directly with the Post model, we need to pass the model name into this Active Record update method. So above, becomes:
 	  @post.update(params.require(:post).permit(:title, :description))
+	  #bc title and description attributes are now nested within the post hash. We need to add the require method. But Rails wants us to be conscious of which attributes we allow to be updated in our database, so we must also permit the title and description in the nested hash. Using strong parameters like this allows ActiveRecord to use mass assignment smoothly
 	  redirect_to post_path(@post)
 	end
 end
