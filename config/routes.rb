ae5679b6b553bbb8ae19fef3d84877c597e6a7b8
zozep <jpark041@gmail.com>
2019-09-04 16:30:13 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #On a high level, PUT has the ability to update the entire object, whereas PATCH simply updates the elements that were changed
+  resources :posts, only: %i[index new show create edit]
+  patch 'posts/:id', to: 'posts#update'
+
 end
